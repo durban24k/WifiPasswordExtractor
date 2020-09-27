a9af297b5054d081cf5fd0116096eee13493744d
Wanjau Alfred Mambo <56720300+durban24k@users.noreply.github.com>
2020-09-27 20:43:11 +0300

    v1.0

--- wifi.py
+++ wifi.py
@@ -1,1 +1,12 @@
 import subprocess
+
+info=subprocess.check_output(['netsh','wlan','show','profiles']).decode('utf-8').split('\n')
+wifi_names=[line.split(':')[1][1:-1] for line in info if "All User Profile" in line]
+
+for wifi_name in wifi_names:
+     results=subprocess.check_output(['netsh','wlan','show','profile',wifi_name,'key=clear']).decode('utf-8').split('\n')
+     results=[line.split(':')[1][1:-1] for line in results if "Key Content" in line]
+     try:
+          print(f'Name: {wifi_name}, Password: {results[0]}')
+     except:
+          print(f'Name: {wifi_name}, Password: Cannot be read!!')
